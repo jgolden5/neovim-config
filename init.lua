77ceee77fc9e8052bd622cca723269b6191ddb8c
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-11 07:21:04 -0700

    added ctrl+w and source init.lua shortcuts using <leader>

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
+vim.g.mapleader = " "
+
 require("colors.catpuccin_simplified")
-require("core.keymaps")
+require("lua.core.keymaps")
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.timeout = false
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,11 @@
 vim.cmd("command! W w") --equivalent to: vim.api.nvim_set_keymap('c', 'W', 'w', { noremap = true }), EXCEPT that in the latter, hitting :W actually displays :w, which it is effectively treated as
+
+vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = "move left window" })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { desc = "move down window" })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { desc = "move up window" })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { desc = "move right window" })
+vim.keymap.set('n', '<leader>r', function()
+  vim.cmd("source ~/.config/nvim/init.lua")
+  print("Sourced nvim init.lua config file")
+end, { desc = "Reload nvim init.lua config" })
+print("keymaps.lua loaded") --
