7cf2dc0b43bcce471242ef954f8676aa0cce7c8a
Jonathan Gurr <jgolden5.tech@gmail.com>
2026-07-08 14:08:06 -0700

    implemented some telescope hotkeys as well as made line number color more visible

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -2,5 +2,6 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
     vim.api.nvim_set_hl(0, "CursorLine", { bg = "#0000ff" })
     vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#00bbff" })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffff00" })
   end,
 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,13 +18,23 @@ return {
           fzf = {}
         }
       }
-      vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags, { desc = "Fuzzy search nvim help" })
-      vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files, { desc = "Fuzzy find files in current working directory" })
       vim.keymap.set("n", "<leader>fs", function()
         require('telescope.builtin').find_files {
           cwd = "~/bd/stretch/stretch-work/"
         }
       end)
+      vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files, { desc = "Fuzzy find files in current working directory" })
+      vim.keymap.set("n", "<leader>ff", function ()
+        require('telescope.builtin').find_files {
+          cwd = "~"
+        }
+      end, { desc = "Fuzzy find files in home directory" })
+      vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags, { desc = "Fuzzy search nvim help" })
+      vim.keymap.set("n", "<leader>fp", function ()
+        require('telescope.builtin').find_files {
+          cwd = '/mnt/c/Users/11045964/OneDrive - BD/Documents/Products'
+        }
+      end, { desc = "Fuzzy find files in windows/.../documents/.../products directory" })
     end
   }
 }
