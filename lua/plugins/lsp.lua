e85e6f2b275bb606349b4b72f30fd13ab1e4e771
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2026-07-13 11:06:11 -0700

    implemented fairly basic lua lsp config with keymaps for diagnostics both inline and entire file summary

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+      vim.lsp.enable("lua_ls")
+      vim.keymap.set("n", "<leader>de", function()
+        vim.diagnostic.open_float(0, {
+          severity = vim.diagnostic.severity.ERROR
+        })
+      end, { desc = "show errors in current buffer" })
+      vim.keymap.set("n", "<leader>dw", function()
+        vim.diagnostic.open_float(0, {
+          severity = vim.diagnostic.severity.WARN
+        })
+      end, { desc = "show warnings in current buffer" })
+      vim.keymap.set("n", "<leader>di", function()
+        vim.diagnostic.open_float(0, {
+          severity = vim.diagnostic.severity.INFO
+        })
+      end, { desc = "show info in current buffer" })
+      vim.keymap.set("n", "<leader>dh", function()
+        vim.diagnostic.open_float(0, {
+          severity = vim.diagnostic.severity.HINT
+        })
+      end, { desc = "show hints in current buffer" })
+
+      vim.keymap.set("n", "<leader>da", function()
+        local diags = vim.diagnostic.get(0)
+        if vim.tbl_isempty(diags) then
+          vim.notify("No diagnostics")
+          return
+        end
+      
+        local lines = {}
+      
+        for _, d in ipairs(diags) do
+          table.insert(
+            lines,
+            string.format(
+              "L%d:C%d %s",
+              d.lnum + 1,
+              d.col + 1,
+              d.message
+            )
+          )
+        end
+      
+        vim.lsp.util.open_floating_preview(
+          lines,
+          "text",
+          { border = "rounded" }
+        )
+      end, {
+        desc = "Show all diagnostics in file",
+      })
+    end,
+  },
+}
