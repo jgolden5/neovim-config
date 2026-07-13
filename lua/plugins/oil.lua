dcf877f3116405bd09f1966f05a0562d2c0966fb
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-07-12 20:31:42 -0700

    improved some oil.lua configs after reading documentation

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,19 +7,29 @@ return {
     lazy = false,
     config = function()
       require("oil").setup {
-        columns = { "icon" },
-        keymaps = {
-          --["<C-h>"] = { "actions.show_help", mode = "n" },
-          --["<M-h>"] = "actions.select.split",
-        },
-        view_options = {
+        columns = {
+          { "icon", add_padding = false },
+        },        
+          view_options = {
           show_hidden = true,
         },
         skip_confirm_for_simple_edits = true,
+        keymaps = {
+          ["-"] = false, --ignore default so I can do my own
+        },
+        float = {
+          padding = 0,
+          max_width = .9999,
+          max_height = .9999,
+          border = nil,
+          win_options = {
+            winblend = 0,
+          },
+        }
       }
-      vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory in current window" })
-
-      vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directory in floating window" })
+      vim.keymap.set("n", "-", require("oil").toggle_float, { desc = "Toggle open parent directory in oil" })
+      --vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directory in floating window" })
     end
   }
 }
+--See more config options here - https://github.com/stevearc/oil.nvim#options
