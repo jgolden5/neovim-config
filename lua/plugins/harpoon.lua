a3b799ea98de9d3c03119e1fb09ad3cb914ffad4
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2026-07-13 14:07:05 -0700

    swapped harpoon add files/view menu keymaps back

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,16 +9,16 @@ return {
       -- REQUIRED
       harpoon:setup()
 
-      --toggle menu
-      vim.keymap.set("n", "<leader>a", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
       --add file
-      vim.keymap.set("n", "<leader>A", function() 
-        harpoon:list():add() 
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
         local current_file = vim.fn.expand("%:t")
         print("added " .. current_file .. " to harpoon list")
       end, { desc = "Add current file to harpoon file list" } )
-      
+
+      --toggle menu
+      vim.keymap.set("n", "<leader>A", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
     end
   },
 }
