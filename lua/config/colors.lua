cf1d537b0c5ecec82caaac40dabd252df139bfea
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2026-07-14 16:14:56 -0700

    added nvim colors for my work-log.codes file

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -5,3 +5,28 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffff00" })
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.codes",
+  callback = function()
+    vim.bo.filetype = "codes"
+
+    vim.cmd([[
+      syntax clear
+      syntax keyword alarisCode ALARIS
+      syntax keyword ilearnCode ILEARN
+      syntax keyword nvimCode NVIM
+      syntax keyword CQ40Code CQ40
+
+      highlight default alarisCode guifg=#ff0000 ctermfg=Red
+      highlight default ilearnCode guifg=#00ff00 ctermfg=Green
+      highlight default nvimCode guifg=#00ffff ctermfg=Cyan
+      highlight default CQ40Code guifg=#008080 ctermfg=6
+    ]])
+
+    vim.cmd("highlight! link redCode redCode")
+    vim.cmd("highlight! link ilearnCode ilearnCode")
+    vim.cmd("highlight! link nvimCode nvimCode")
+    vim.cmd("highlight! link CQ40Code CQ40Code")
+  end,
+  })
