867074c909b7be4e815d85feef099db456523551
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2026-07-14 16:26:59 -0700

    added undotree plugin

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Toggle Undotree" },
+    },
+    cmd = { "UndotreeToggle", "UndotreeShow", "UndotreeHide", "UndotreeFocus" },
+  },
+}
