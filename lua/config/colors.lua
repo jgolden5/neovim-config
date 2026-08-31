fe86abc3defe50895bbdc921e6796936b383dfa8
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2026-08-31 13:08:35 -0700

    added pinkish/purple and gray colors for recon and denali respectively (.codes file)

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -34,6 +34,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     syntax keyword crosserCode CROSSER
     syntax keyword trueflowCode TRUEFLOW
     syntax keyword tutorCode TUTOR
+    syntax keyword reconCode RECON
+    syntax keyword denaliCode DENALI
 
     highlight default planCode guifg=#9010F0 ctermfg=DarkMagenta
     highlight default alarisCode guifg=#ff0000 ctermfg=Red
@@ -44,6 +46,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     highlight default crosserCode guifg=#ffbb00 ctermfg=215
     highlight default trueflowCode guifg=#ff9900 ctermfg=214
     highlight default tutorCode guifg=#dd8800 ctermfg=94
+    highlight default reconCode guifg=#aa00ff ctermfg=181
+    highlight default denaliCode guifg=#999999 ctermfg=Gray
     ]])
 
   end,
